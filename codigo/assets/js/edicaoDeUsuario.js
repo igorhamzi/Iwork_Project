792e487040a1c086492c4c9b319fc045dd58f7c2
Igor Hamzi <igorwhamzi@gmail.com>
2023-12-12 17:50:37 -0300

    update: edicaoDeUsuario LocalStorage

--- codigo/assets/js/edicaoDeUsuario.js
+++ codigo/assets/js/edicaoDeUsuario.js
@@ -1,100 +1,92 @@
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-  }
-  
-  window.onload = function() {
-    if (!localStorage.getItem("nomeUsuario")) {
-      localStorage.setItem("nomeUsuario", "Gabriel Dias");
-    }
-  
-    if (!localStorage.getItem("emailUsuario")) {
-      localStorage.setItem("emailUsuario", "gabriel.dias@outlook.com");
-    }
-  
-    if (!localStorage.getItem("avaliacaoUsuario")) {
-      localStorage.setItem("avaliacaoUsuario", "Aluno de SI");
-    }
-  
-    if (!localStorage.getItem("enderecoUsuario")) {
-      localStorage.setItem("enderecoUsuario", "Rua Diógenes Roma - Jaboticabal - SP");
-    }
-  
-    document.getElementById("title_nomeUsuario").textContent = "Nome: " + localStorage.getItem("nomeUsuario");
-    document.getElementById("title_emailUsuario").textContent = "E-mail: " + localStorage.getItem("emailUsuario");
-    document.getElementById("title_avaliacaoUsuario").textContent = "Avaliação: " + localStorage.getItem("avaliacaoUsuario");
-    document.getElementById("title_enderecoUsuario").textContent = "Endereço: " + localStorage.getItem("enderecoUsuario");
-  };
-  
-  function editarUsuario() {
-    document.getElementById("nomeUsuario").style.display = "inline";
-    document.getElementById("emailUsuario").style.display = "inline";
-    document.getElementById("avaliacaoUsuario").style.display = "inline";
-    document.getElementById("enderecoUsuario").style.display = "inline";
-  
-    document.getElementById("nomeUsuario").value = localStorage.getItem("nomeUsuario");
-    document.getElementById("emailUsuario").value = localStorage.getItem("emailUsuario");
-    document.getElementById("avaliacaoUsuario").value = localStorage.getItem("avaliacaoUsuario");
-    document.getElementById("enderecoUsuario").value = localStorage.getItem("enderecoUsuario");
-  
-    document.getElementById("imageLink").addEventListener("click", function() {
-      const imageInput = document.createElement("input");
-      imageInput.type = "file";
-      imageInput.accept = "image/*";
-      imageInput.style.display = "none";
-  
-      imageInput.addEventListener("change", function(event) {
-        const imageFile = event.target.files[0];
-        const savedImage = document.getElementById("savedImage");
-        const reader = new FileReader();
-  
-        reader.onload = function() {
-          savedImage.src = reader.result;
-          document.getElementById("imageContainer").style.display = "block";
-          localStorage.setItem("uploadedImageUsuario", reader.result);
-        };
-  
-        if (imageFile) {
-          reader.readAsDataURL(imageFile);
-        }
-      });
-  
-      document.body.appendChild(imageInput);
-      imageInput.click();
+window.onload = function() {
+  const usuarioAtivo = JSON.parse(localStorage.getItem('loginAtivo')) || {};
+
+  document.getElementById("title_nomeUsuario").textContent = "Nome: " + usuarioAtivo.nome;
+  document.getElementById("title_emailUsuario").textContent = "E-mail: " + usuarioAtivo.email;
+  document.getElementById("title_avaliacaoUsuario").textContent = "Telefone: " + usuarioAtivo.telefone;
+  document.getElementById("title_enderecoUsuario").textContent = "Endereço: " + usuarioAtivo.endereco;
+};
+
+function editarUsuario() {
+  const usuarioAtivo = JSON.parse(localStorage.getItem('loginAtivo')) || {};
+
+  const inputs = ["nomeUsuario", "emailUsuario", "avaliacaoUsuario", "enderecoUsuario"];
+  inputs.forEach(inputId => {
+    document.getElementById(inputId).style.display = "inline";
+    document.getElementById(inputId).value = usuarioAtivo[inputId.replace("Usuario", "")];
+  });
+
+  setupImageUpload();
+}
+
+function setupImageUpload() {
+  document.getElementById("imageLink").addEventListener("click", function () {
+    const imageInput = document.createElement("input");
+    imageInput.type = "file";
+    imageInput.accept = "image/*";
+    imageInput.style.display = "none";
+
+    imageInput.addEventListener("change", function (event) {
+      const imageFile = event.target.files[0];
+      const savedImage = document.getElementById("savedImage");
+      const reader = new FileReader();
+
+      reader.onload = function () {
+        savedImage.src = reader.result;
+        document.getElementById("imageContainer").style.display = "block";
+        localStorage.setItem("uploadedImageUsuario", reader.result);
+      };
+
+      if (imageFile) {
+        reader.readAsDataURL(imageFile);
+      }
     });
-  
-    const savedImage = document.getElementById("savedImage");
-    const imageBase64 = localStorage.getItem("uploadedImageUsuario");
-  
-    if (imageBase64) {
-      savedImage.src = imageBase64;
-      document.getElementById("imageContainer").style.display = "block";
-    }
+
+    document.body.appendChild(imageInput);
+    imageInput.click();
+  });
+
+  const savedImage = document.getElementById("savedImage");
+  const imageBase64 = localStorage.getItem("uploadedImageUsuario");
+
+  if (imageBase64) {
+    savedImage.src = imageBase64;
+    document.getElementById("imageContainer").style.display = "block";
   }
-  
-  function salvarUsuario() {
-    var nomeEmpresa = document.getElementById("nomeUsuario").value;
-    var email = document.getElementById("emailUsuario").value;
-    var avaliacaoUsuario = document.getElementById("avaliacaoUsuario").value;
-    var endereco = document.getElementById("enderecoUsuario").value;
-  
-    localStorage.setItem("nomeUsuario", nomeEmpresa);
-    localStorage.setItem("emailUsuario", email);
-    localStorage.setItem("avaliacaoUsuario", avaliacaoUsuario);
-    localStorage.setItem("enderecoUsuario", endereco);
-  
-    var nomeArmazenado = localStorage.getItem("nomeUsuario");
-    var emailArmazenado = localStorage.getItem("emailUsuario");
-    var avaliacaoUsuarioArmazenado = localStorage.getItem("avaliacaoUsuario");
-    var enderecoArmazenado = localStorage.getItem("enderecoUsuario");
-  
-    document.getElementById("nomeUsuario").style.display = "none";
-    document.getElementById("emailUsuario").style.display = "none";
-    document.getElementById("avaliacaoUsuario").style.display = "none";
-    document.getElementById("enderecoUsuario").style.display = "none";
-  
-    document.getElementById("title_nomeUsuario").textContent = "Nome: " + nomeArmazenado;
-    document.getElementById("title_emailUsuario").textContent = "E-mail: " + emailArmazenado;
-    document.getElementById("title_avaliacaoUsuario").textContent = "Avaliação: " + avaliacaoUsuarioArmazenado;
-    document.getElementById("title_enderecoUsuario").textContent = "Endereço: " + enderecoArmazenado;
-  }
+}
+
+function salvarUsuario() {
+  const usuarioAtivo = JSON.parse(localStorage.getItem('loginAtivo')) || {};
+
+  var nomeUsuario = document.getElementById("nomeUsuario").value;
+  var email = document.getElementById("emailUsuario").value;
+  var telefoneUsuario = document.getElementById("avaliacaoUsuario").value; 
+  var endereco = document.getElementById("enderecoUsuario").value;
+
+  usuarioAtivo.nome = nomeUsuario;
+  usuarioAtivo.email = email;
+  usuarioAtivo.telefone = telefoneUsuario;
+  usuarioAtivo.endereco = endereco;
+
+  localStorage.setItem('loginAtivo', JSON.stringify(usuarioAtivo));
+
+  localStorage.setItem("nomeUsuario", nomeUsuario);
+  localStorage.setItem("emailUsuario", email);
+  localStorage.setItem("avaliacaoUsuario", telefoneUsuario); 
+  localStorage.setItem("enderecoUsuario", endereco);
+
+  var nomeArmazenado = localStorage.getItem("nomeUsuario");
+  var emailArmazenado = localStorage.getItem("emailUsuario");
+  var telefoneUsuarioArmazenado = localStorage.getItem("avaliacaoUsuario");  
+  var enderecoArmazenado = localStorage.getItem("enderecoUsuario");
+
+  const inputs = ["nomeUsuario", "emailUsuario", "avaliacaoUsuario", "enderecoUsuario"];
+  inputs.forEach(inputId => {
+    document.getElementById(inputId).style.display = "none";
+  });
+
+  document.getElementById("title_nomeUsuario").textContent = "Nome: " + nomeArmazenado;
+  document.getElementById("title_emailUsuario").textContent = "E-mail: " + emailArmazenado;
+  document.getElementById("title_avaliacaoUsuario").textContent = "Telefone: " + telefoneUsuarioArmazenado;  // Alterado para telefone
+  document.getElementById("title_enderecoUsuario").textContent = "Endereço: " + enderecoArmazenado;
+}
